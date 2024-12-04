50c3bb95b4694a996c6350d38750f9a2692e3612
trisfaleel <dvirus4337@gmail.com>
2024-12-03 23:21:34 -0800

    made saving and loading of game(s)

--- header/triviaGame.hpp
+++ header/triviaGame.hpp
@@ -16,4 +16,10 @@ class triviaGame {
     void addQuestion (const std::string& newQuestion) { question.push_back(newQuestion); }
     void addOptions (std::vector<std::string> newOptions) { options.push_back(newOptions); }
     void addAnswer (const std::string& newAnswer) { answer.push_back(newAnswer); }
+    int numberOfQuestions() { return question.size(); }
+    const string& getTitle() { return title; }
+    int getType(int questionIndex) { return type.at(questionIndex); }
+    const string& getQuestion(int questionIndex) { return question.at(questionIndex); }
+    const vector<string>& getOptions(int questionIndex) { return options.at(questionIndex); }
+    const string& getAnswer (int questionIndex) { return answer.at(questionIndex); }
 };
